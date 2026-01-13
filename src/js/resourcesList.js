ed87f4dbc60dc5e5d2e749ef55e69af86df99951
Broderick Nana Bentil <bentilfrederick4@gmail.com>
2026-01-13 17:18:16 +0000

    ui: implement mobile and tablet responsiveness

--- src/js/resourcesList.js
+++ src/js/resourcesList.js
@@ -1,11 +1,11 @@
-import { allResources } from './main';
+import { allResources } from "./main";
 
 export function resourceList() {
-  const dashboard = document.getElementById('main-content');
+  const dashboard = document.getElementById("main-content");
 
   for (const category in allResources) {
-    const section = document.createElement('secton');
-    section.className = 'dashboard__category';
+    const section = document.createElement("secton");
+    section.className = "dashboard__category";
 
     section.innerHTML = `
     <h2>${category.toUpperCase()}</h2>
@@ -20,12 +20,12 @@ export function resourceList() {
         <a class="dashboard__category-item-link" href="${resources.url}" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
 </svg>
-${resources.url} </a>
+${resources.name} </a>
         </section>
         
-        `,
+        `
       )
-      .join('')}
+      .join("")}
     
     </section>
     
